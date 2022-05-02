e5774974d4e1116d58a887808c88775b265a03b0
AirichDiawan <93786064+AirichDiawan@users.noreply.github.com>
2022-05-02 17:11:47 +0800

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="img/png" href="ImagesPlaceholder/logo1.jpg">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+	<title>Garb by Shin</title>
+</head>
+<body>
+<!--------------------NAVBAR-------------------->
+	<nav id="navbar" class="navbar">
+		<div class="navlinks">
+			<div class="leftlinks">
+				<!--<div class="logo"><img alt="Logo" src="Images/logoIcons/logo.png"></div>-->
+				<a class="links home" href="#origBody" id="activeLink">Men</a>
+				<a class="links menu" href="#origBody">Women</a>
+				<a class="links about" href="#origBody">Kids</a>
+			</div>
+			<div class="rightLinks">
+				<a href="#"><i class='fa fa-user' id="userIcon"></i></a>
+				<a href="#"><i class="fa" id="heart">&#xf004;</i></a>
+				<a href="#"><i class='fa fa-shopping-bag' id="shoppingBag"></i></a>
+			</div>
+		</div>
+	</nav>
+<!--------------------BODY-------------------->
+	<section class="body1">
+		<p class="bannerTitle">BANNER HERE</p>
+	</section>
+
+
+	<section class="body2">
+		<p class="title">Category</p>
+		<div class="b2c1">	
+			<div class="contentBoxMenu">
+				<div class="boxContMenu">
+					<div class="boxPicsMenu">
+						<img alt="Chickens" class="ContImageMenu" src="ImagesPlaceholder/longSleeve.png">
+					</div>
+					<div class="boxTextMenu">
+						<p class="categoryTitle">Mens</p>
+					</div>
+				</div>		
+				<div class="boxContMenu">
+					<div class="boxPicsMenu">
+						<img alt="Pancits" class="ContImageMenu" src="ImagesPlaceholder/womenShirt.png">
+					</div>
+					<div class="boxTextMenu">
+						<p class="categoryTitle">Womens</p>
+					</div>
+				</div>
+				<div class="boxContMenu">
+					<div class="boxPicsMenu">
+						<img alt="Pancits" class="ContImageMenu" src="ImagesPlaceholder/kidShirt.png">
+					</div>
+					<div class="boxTextMenu">
+						<p class="categoryTitle">Kids</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="contentBoxMenu cbmbtn1">
+			<a class="btn1" href="#">See all in Shop</a>
+		</div>
+
+	</section>
+
+	<section class="body3">
+		<div class="b3slideImage">
+			<p class="title">Trending</p>
+			<button class="pre-btn"><img src="ImagesPlaceholder/slider/arrow.png"></button>
+			<button class="nxt-btn"><img src="ImagesPlaceholder/slider/arrow.png"></button>
+			<div class="sliderContainer">
+				<div class="box">
+					<div class="slideImage">
+						<img src="ImagesPlaceholder/slider/1.jpg" alt="pic1">
+					</div>
+					<div class="detailBox">
+						<div class="itemType">
+							<a href="#">Item 1</a>
+						</div>
+						<div class="itemPrice">
+							<p>₱499</p>
+						</div>
+					</div>
+					<div class="btnBox">
+						<a href="#">Add to bag</a>
+					</div>
+				</div>
+
+				<div class="box">
+					<div class="slideImage">
+						<img src="ImagesPlaceholder/slider/2.jpg" alt="pic2">
+					</div>
+					<div class="detailBox">
+						<div class="itemType">
+							<a href="#">Item 2</a>
+						</div>
+						<div class="itemPrice">
+							<p>₱499</p>
+						</div>
+					</div>
+					<div class="btnBox">
+						<a href="#">Add to bag</a>
+					</div>
+				</div>
+
+				<div class="box">
+					<div class="slideImage">
+						<img src="ImagesPlaceholder/slider/3.jpg" alt="pic3">
+					</div>
+					<div class="detailBox">
+						<div class="itemType">
+							<a href="#">Item 3</a>
+						</div>
+						<div class="itemPrice">
+							<p>₱499</p>
+						</div>
+					</div>
+					<div class="btnBox">
+						<a href="#">Add to bag</a>
+					</div>
+				</div>
+
+				<div class="box">
+					<div class="slideImage">
+						<img src="ImagesPlaceholder/slider/4.jpg" alt="pic4">
+					</div>
+					<div class="detailBox">
+						<div class="itemType">
+							<a href="#">Item 4</a>
+						</div>
+						<div class="itemPrice">
+							<p>₱499</p>
+						</div>
+					</div>
+					<div class="btnBox">
+						<a href="#">Add to bag</a>
+					</div>
+				</div>
+
+				<div class="box">
+					<div class="slideImage">
+						<img src="ImagesPlaceholder/slider/5.jpg" alt="pic5">
+					</div>
+					<div class="detailBox">
+						<div class="itemType">
+							<a href="#">Item 5</a>
+						</div>
+						<div class="itemPrice">
+							<p>₱499</p>
+						</div>
+					</div>
+					<div class="btnBox">
+						<a href="#">Add to bag</a>
+					</div>
+				</div>
+
+				<div class="box">
+					<div class="slideImage">
+						<img src="ImagesPlaceholder/slider/6.jpg" alt="pic6">
+					</div>
+					<div class="detailBox">
+						<div class="itemType">
+							<a href="#">Item 6</a>
+						</div>
+						<div class="itemPrice">
+							<p>₱499</p>
+						</div>
+					</div>
+					<div class="btnBox">
+						<a href="#">Add to bag</a>
+					</div>
+				</div>
+
+				<div class="box">
+					<div class="slideImage">
+						<img src="ImagesPlaceholder/slider/7.jfif" alt="pic7">
+					</div>
+					<div class="detailBox">
+						<div class="itemType">
+							<a href="#">Item 7</a>
+						</div>
+						<div class="itemPrice">
+							<p>₱499</p>
+						</div>
+					</div>
+					<div class="btnBox">
+						<a href="#">Add to bag</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="contentBoxMenu cbmbtn1">
+			<a class="btn1" href="#">See all in Shop</a>
+		</div>
+	</section>
+
+	<section class="footer">
+		<p class="bannerTitle">FOOTER</p>
+	</section>
+	<script type="text/javascript" src="script1.js"></script>
+</body>
+</html>
---
+++ script1.js
@@ -0,0 +1,15 @@
+const productContainers = [...document.querySelectorAll('.sliderContainer')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+});
